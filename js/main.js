a0721da6c1d0d7b331eb3f3092332ddafa7dcab4
Antonranestam <anton@ranestam.se>
2016-10-12 17:02:38 +0200

    Added foundation for website

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Import libs
+import $ from 'jquery';
+import utils from 'lib/utils';
+
+(function () {
+  // Run when DOM is ready
+  $(function () {
+    // If SVG is not supported replace it with png version
+    utils.replaceSVG();
+  });
+})();
