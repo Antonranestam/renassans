078b839512bc9250a738f402bfd376c8c72379e9
Antonranestam <anton@ranestam.se>
2016-10-15 15:49:31 +0200

    Changes to music player

--- public/dist/js/bundle.js
+++ public/dist/js/bundle.js
@@ -1,6 +1,32 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
-$(document).ready(function () {});
+$(document).ready(function () {
+   $('#music-player__mute').click(function () {
+      toggleMute();
+   });
+
+   initialiseMediaPlayer();
+});
+
+var mediaPlayer;
+
+// Init media player
+function initialiseMediaPlayer() {
+   mediaPlayer = document.getElementById('music');
+   mediaPlayer.controls = false;
+   mediaPlayer.load();
+}
+
+// Toggle mute for music
+function toggleMute() {
+   var btn = document.getElementById('music-player__mute');
+
+   if (mediaPlayer.muted) {
+      mediaPlayer.muted = false;
+   } else {
+      mediaPlayer.muted = true;
+   }
+}
 
 },{}]},{},[1]);
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
+  $('#music-player__mute').click(function () {
+    toggleMute();
+  })
 
+  initialiseMediaPlayer();
 });
+
+var mediaPlayer;
+
+// Init media player
+function initialiseMediaPlayer() {
+   mediaPlayer = document.getElementById('music');
+   mediaPlayer.controls = false;
+   mediaPlayer.load();
+}
+
+// Toggle mute for music
+function toggleMute() {
+   var btn = document.getElementById('music-player__mute');
+
+   if (mediaPlayer.muted) {
+      mediaPlayer.muted = false;
+   }
+   else {
+      mediaPlayer.muted = true;
+   }
+}
