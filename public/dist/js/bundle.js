d87eb1c98dfc69ef1d476bb409a47f8959efb961
Antonranestam <anton@ranestam.se>
2016-10-15 16:54:46 +0200

    Added analyser for music track

--- public/dist/js/bundle.js
+++ public/dist/js/bundle.js
@@ -3,39 +3,65 @@
 
 $(document).ready(function () {
 
-   $('#music-player__mute').click(function () {
-      toggleMute();
-   });
+  $('#music-player__mute').click(function () {
+    toggleMute();
+  });
 
-   initialiseMediaPlayer();
+  initialiseMediaPlayer();
 
-   progressBar();
+  progressBar();
 });
 
 var mediaPlayer;
 
 // Init media player
 function initialiseMediaPlayer() {
-   mediaPlayer = document.getElementById('music');
-   mediaPlayer.controls = false;
-   mediaPlayer.load();
+  mediaPlayer = document.getElementById('music');
+  mediaPlayer.controls = false;
+  mediaPlayer.load();
 }
 
 // Toggle mute for music
 function toggleMute() {
 
-   if (mediaPlayer.muted) {
-      mediaPlayer.muted = false;
-   } else {
-      mediaPlayer.muted = true;
-   }
+  if (mediaPlayer.muted) {
+    mediaPlayer.muted = false;
+  } else {
+    mediaPlayer.muted = true;
+  }
 }
 
 // Update progress bar
 function progressBar() {
-   $('#music').on('timeupdate', function () {
-      $('#progress-bar').attr("value", 100 * this.currentTime / this.duration);
-   });
+  $('#music').on('timeupdate', function () {
+    $('#progress-bar').attr("value", 100 * this.currentTime / this.duration);
+  });
 }
+
+// Analyser node
+window.onload = function () {
+  var ctx = new AudioContext();
+  var audio = document.getElementById('music');
+  var audioSrc = ctx.createMediaElementSource(audio);
+  var analyser = ctx.createAnalyser();
+  // we have to connect the MediaElementSource with the analyser
+  audioSrc.connect(analyser);
+  audioSrc.connect(ctx.destination);
+  // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
+
+  // frequencyBinCount tells you how many values you'll receive from the analyser
+  var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+  // we're ready to receive some data!
+  // loop
+  function renderFrame() {
+    requestAnimationFrame(renderFrame);
+    // update data in frequencyData
+    analyser.getByteFrequencyData(frequencyData);
+    // render frame based on values in frequencyData
+    // console.log(frequencyData)
+  }
+  renderFrame();
+};
 
 },{}]},{},[1]);
--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
   initialiseMediaPlayer();
 
   progressBar();
+
 });
 
 var mediaPlayer;
@@ -35,3 +36,29 @@ function progressBar() {
     $('#progress-bar').attr("value", 100 * this.currentTime / this.duration);
   });
 }
+
+// Analyser node
+window.onload = function() {
+  var ctx = new AudioContext();
+  var audio = document.getElementById('music');
+  var audioSrc = ctx.createMediaElementSource(audio);
+  var analyser = ctx.createAnalyser();
+  // we have to connect the MediaElementSource with the analyser
+  audioSrc.connect(analyser);
+  audioSrc.connect(ctx.destination);
+  // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
+
+  // frequencyBinCount tells you how many values you'll receive from the analyser
+  var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+  // we're ready to receive some data!
+  // loop
+  function renderFrame() {
+     requestAnimationFrame(renderFrame);
+     // update data in frequencyData
+     analyser.getByteFrequencyData(frequencyData);
+     // render frame based on values in frequencyData
+     // console.log(frequencyData)
+  }
+  renderFrame();
+};
